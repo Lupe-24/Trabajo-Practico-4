dc948c90db01b9c57d3a903396b817aac87b0966
Lupe-24 <naiaraayelen129@gmail.com>
2025-09-01 19:27:51 -0300

    actualización en clase Alumno

--- TrabajoPractico4/src/Vistas/Alumno.java
+++ TrabajoPractico4/src/Vistas/Alumno.java
@@ -11,4 +11,56 @@ package Vistas;
  */
 public class Alumno {
     
+    private int legajo;
+    private String apellido;
+    private String nombre;
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void agregarMateria(Materias m){
+        
+    }
+    
+    public int cantidadMaterias(){        
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno " + apellido + nombre + ""
+                + "\n\tNúmero de legajo " + legajo;
+    }
+    
+    
+    
+    
+    
 }
